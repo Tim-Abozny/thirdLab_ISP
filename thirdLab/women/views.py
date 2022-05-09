331b735f888db17ff62f9f7fb5db2cdfaa3ae039
Tim-Abozny <tim.obozny@gmail.com>
2022-05-09 20:16:52 +0300

    Work with Class-Based Views

--- thirdLab/women/views.py
+++ thirdLab/women/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.views.generic import ListView, DetailView, CreateView
 from .forms import *
 from .models import *
 
@@ -12,32 +12,59 @@ menu = [
 ]
 
 
-def index(request):
-    posts = Women.objects.all()
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
 
-    send_parameters = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Main page',
-        'cat_selected': 0
-    }
-    return render(request, 'women/index.html', context=send_parameters)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Main page'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
+
+# def index(request):
+#    posts = Women.objects.all()
+#
+#    send_parameters = {
+#        'posts': posts,
+#        'menu': menu,
+#        'title': 'Main page',
+#        'cat_selected': 0
+#    }
+#    return render(request, 'women/index.html', context=send_parameters)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About'})
 
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Add article'})
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Adding article'
+        context['cat_selected'] = 0
+        return context
+
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Add article'})
 
 
 def contact(request):
@@ -52,29 +79,59 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found :(</h1>')
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Women, slug=post_slug)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Women, slug=post_slug)
+#
+#     send_parameters = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'women/post.html', context=send_parameters)
 
-    send_parameters = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
 
-    return render(request, 'women/post.html', context=send_parameters)
+class ShowPost(DetailView):
+    model = Women
+    template_name = 'women/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = context['post']
+        return context
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    if len(posts) == 0:
-        raise Http404
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    send_parameters = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Display by category',
-        'cat_selected': cat_id
-    }
-    return render(request, 'women/index.html', context=send_parameters)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Category - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+
+# def show_category(request, cat_id):
+#     posts = Women.objects.filter(cat_id=cat_id)
+#
+#     if len(posts) == 0:
+#         raise Http404
+#
+#     send_parameters = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Display by category',
+#         'cat_selected': cat_id
+#     }
+#     return render(request, 'women/index.html', context=send_parameters)
