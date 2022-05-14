b554769094e05ffd9bb86ea5b9485e12e0ed38d4
Tim-Abozny <tim.obozny@gmail.com>
2022-05-14 13:08:51 +0300

    Created user authorization

--- thirdLab/women/views.py
+++ thirdLab/women/views.py
@@ -1,6 +1,8 @@
 from builtins import set
 
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -26,18 +28,6 @@ class WomenHome(DataMixin, ListView):
         return Women.objects.filter(is_published=True)
 
 
-# def index(request):
-#    posts = Women.objects.all()
-#
-#    send_parameters = {
-#        'posts': posts,
-#        'menu': menu,
-#        'title': 'Main page',
-#        'cat_selected': 0
-#    }
-#    return render(request, 'women/index.html', context=send_parameters)
-
-
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About'})
 
@@ -53,41 +43,13 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         c_def = self.get_user_context(title="Adding article")
         return dict(list(context.items()) + list(c_def.items()))
 
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # print(form.cleaned_data)
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Add article'})
-
 
 def contact(request):
     return HttpResponse("Feedback")
 
 
-def login(request):
-    return HttpResponse("Authorization")
-
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found :(</h1>')
-
-
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Women, slug=post_slug)
-#
-#     send_parameters = {
-#         'post': post,
-#         'menu': menu,
-#         'title': post.title,
-#         'cat_selected': post.cat_id,
-#     }
-#
-#     return render(request, 'women/post.html', context=send_parameters)
 
 
 class ShowPost(DataMixin, DetailView):
@@ -118,21 +80,6 @@ class WomenCategory(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
-# def show_category(request, cat_id):
-#     posts = Women.objects.filter(cat_id=cat_id)
-#
-#     if len(posts) == 0:
-#         raise Http404
-#
-#     send_parameters = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': 'Display by category',
-#         'cat_selected': cat_id
-#     }
-#     return render(request, 'women/index.html', context=send_parameters)
-
-
 class RegisterUser(DataMixin, CreateView):
     form_class = RegisterUserForm
     template_name = 'women/register.html'
@@ -142,3 +89,25 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Registration")
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Authorization")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
